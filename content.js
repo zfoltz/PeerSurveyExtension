f400decd6b67bea6c1fae881bfdc6fbb0c8d247d
Zach Foltz <zfoltz3@gmail.com>
2023-09-22 08:35:18 -0600

    added difficulty calculation, sorting html table, caching, and buttons on the popup for management

--- content.js
+++ content.js
@@ -5,6 +5,144 @@ function storeSurveyCount(surveyCount) {
     chrome.storage.local.set({ surveyCount });
   }
 
+// Function to store survey difficulty in local storage
+function storeSurveyDifficulty(surveyId, difficulty) {
+  chrome.storage.local.set({ [surveyId]: difficulty });
+}
+
+// Function to retrieve survey difficulty from local storage
+async function getSurveyDifficulty(surveyId) {
+  return new Promise((resolve) => {
+    chrome.storage.local.get([surveyId], (result) => {
+      resolve(result[surveyId]);
+    });
+  });
+}
+
+// Function to calculate the difficulty of a surveys questions
+function calculateDifficulty(surveyQuestions) {
+  let totalPoints = 0;
+  surveyQuestions.forEach((question) => {
+    if (question.required) {
+      if (question.type == "text") {
+        totalPoints += 7;
+      } else if (question.type == "checkbox") {
+        totalPoints += 2;
+      } else if (question.type == "radio") {
+        totalPoints += 1;
+      }
+      }
+    });
+    return totalPoints;
+}
+
+// Function to fetch the difficulty for a single survey
+async function fetchSurveyDifficulty(token, surveyId) {
+  try {
+    // Check if the difficulty is already cached
+    const cachedDifficulty = await getSurveyDifficulty(surveyId);
+    
+    if (cachedDifficulty !== undefined) {
+      // Return the cached difficulty if available
+      return cachedDifficulty;
+    }
+
+    // If not cached, make the API request
+    const apiUrl = 'http://peersurvey.cc.gatech.edu/api/v1/survey/respond-questions';
+    
+    // Make the POST request for a single survey
+    const response = await fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ id: surveyId }),
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      const survey = JSON.parse(data.payload);
+      const difficultyScore = calculateDifficulty(survey.questions);
+
+      // Store the difficulty score in cache
+      storeSurveyDifficulty(surveyId, difficultyScore);
+
+      return difficultyScore;
+    } else {
+      console.error(`Failed to fetch survey difficulty for survey ID ${surveyId}:`, response.statusText);
+      return 0; // Return 0 if there's an error
+    }
+  } catch (error) {
+    console.error(`Error fetching survey difficulty for survey ID ${surveyId}:`, error);
+    return 0; // Return 0 if there's an error
+  }
+}
+
+// Function to add difficulty scores to each row in the table
+async function addDifficultyScoresToTable(token, openSurveys) {
+  try {
+    // Retrieve the list of survey IDs from your data (replace with your data)
+    const surveyIds = openSurveys.slice(0,10).map(obj => obj.id);
+
+    // Use Promise.all to parallelize the requests
+    const difficultyScores = await Promise.all(
+      surveyIds.map((surveyId) => fetchSurveyDifficulty(token, surveyId))
+    );
+
+    // Add a header to the table
+    const tableHeader = document.querySelector('#tblResponse thead tr');
+    const difficultyHeader = document.createElement('th');
+    difficultyHeader.textContent = 'Difficulty';
+    difficultyHeader.className = 'Difficulty';
+    tableHeader.appendChild(difficultyHeader);
+
+    // Add difficulty scores to each row in the table
+    const tableRows = document.querySelectorAll('#tblResponse tbody tr');
+    tableRows.forEach((row, index) => {
+      const difficulty = difficultyScores[index] || '999'; // Use 'N/A' if no difficulty is available
+      const difficultyCell = document.createElement('td');
+      difficultyCell.textContent = difficulty;
+      difficultyCell.className = 'Difficulty';
+      row.appendChild(difficultyCell);
+    });
+
+    // Now you have added the difficulty scores to each row in the table
+  } catch (error) {
+    console.error('Error adding difficulty scores to the table:', error);
+  }
+}
+
+// Function to add difficulty score as a data attribute to each row
+function addDifficultyToRows() {
+  const tableRows = document.querySelectorAll('#tblResponse tbody tr');
+  tableRows.forEach((row) => {
+    // Extract the difficulty score from the "difficulty" column
+    const difficultyScore = parseInt(row.querySelector('.Difficulty').textContent, 10);
+    
+    // Add the difficulty score as a data attribute
+    row.setAttribute('data-difficulty', difficultyScore);
+  });
+}
+
+// Function to sort the table by difficulty (ascending order)
+function sortTableByDifficulty() {
+  const tableBody = document.querySelector('#tblResponse tbody');
+  const rows = Array.from(tableBody.querySelectorAll('tr'));
+
+  // Sort the rows based on the data-difficulty attribute
+  rows.sort((a, b) => {
+    const difficultyA = parseInt(a.getAttribute('data-difficulty'), 10);
+    const difficultyB = parseInt(b.getAttribute('data-difficulty'), 10);
+    return difficultyA - difficultyB;
+  });
+
+  // Append sorted rows back to the table
+  rows.forEach((row) => {
+    tableBody.appendChild(row);
+  });
+}
+
 // Function to make the API request
 async function makeApiRequest(token) {
     try {
@@ -15,19 +153,25 @@ async function makeApiRequest(token) {
         method: 'POST',  // Adjust the method as needed
         headers: {
           'Authorization': `Bearer ${token}`,
-          'Content-Type': 'application/json', // Adjust content type as needed
+          'Content-Type': 'application/json', 
         },
-        body: JSON.stringify({}), // Include the request payload if necessary
+        body: JSON.stringify({}), 
       });
   
       if (response.ok) {
         const data = await response.json();
         // Process the data as needed
-        console.log(data)
-        storeSurveyCount(data.courseOpenSurveyList.length);
-        const surveyCount = data.courseOpenSurveyList.length; // Adjust this to your data structure
-        console.log('Survey Count:', surveyCount);
-        // Now you can display the count or perform any other actions
+        let surveys = JSON.parse(data.payload);
+        const openSurveys = surveys.courseOpenSurveyList;
+        
+        const openSurveyCount = openSurveys.length; // Adjust this to your data structure
+        storeSurveyCount(openSurveyCount);
+
+        // Add difficulty scores to each row in the table
+        await addDifficultyScoresToTable(token, openSurveys);
+        addDifficultyToRows();
+        sortTableByDifficulty();
+      
       } else {
         console.error('Failed to fetch survey data:', response.statusText);
       }
@@ -38,23 +182,19 @@ async function makeApiRequest(token) {
   
   // Send a message to the background script to request the token
   function main() {
-    console.log('content.js main() called')
     chrome.runtime.sendMessage({ action: 'getToken' }, (response) => {
-        console.log("response is: ", response)
         if (response && response.token) {
-        // If the token is available, make the API request
-        makeApiRequest(response.token);
-      }
+          // If the token is available, make the API request
+          makeApiRequest(response.token);
+        }
     });
   }
 
 
   if (document.readyState !== 'loading') {
-    console.log('document is already ready, just execute code here');
     main();
 } else {
     document.addEventListener('DOMContentLoaded', function () {
-        console.log('document was not ready, place code here');
         main();
     });
 }
--- popup.js
+++ popup.js
@@ -1,5 +1,15 @@
 // popup.js
 document.addEventListener('DOMContentLoaded', function () {
+    // Add a click event listener to the "Clear Cache" button
+    const clearCacheButton = document.getElementById('clearCacheButton');
+
+    clearCacheButton.addEventListener('click', function () {
+      // Clear the cache in local storage
+      chrome.storage.local.clear(function () {
+        console.log('Cache cleared.');
+      });
+    });
+
     // Fetch the survey count from local storage (if available)
     chrome.storage.local.get('surveyCount', function (data) {
       const surveyCount = data.surveyCount || 'N/A';
